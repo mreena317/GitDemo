5e563ea05323bbccb9cabceed802616d55c66694
reena <mreena317@gmail.com>
2022-08-16 14:22:36 -0400

    first commit

---
+++ src/test/java/mavenpractice/mavenJava/SeleniumTest.java
@@ -0,0 +1,15 @@
+package mavenpractice.mavenJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("BrowserAutomation");
+	}
+
+	@Test
+	public void elementsUI() {
+		System.out.println("elementsUI");
+	}
+}
